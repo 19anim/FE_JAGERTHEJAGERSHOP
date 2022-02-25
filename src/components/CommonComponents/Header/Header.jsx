510c0e84a115e95b8426a5452094dc4815df58ac
Nguyễn Phi Tuấn Ân <npta@netcompany.com>
2022-02-25 13:03:31 +0700

    Header responsive

--- src/components/CommonComponents/Header/Header.jsx
+++ src/components/CommonComponents/Header/Header.jsx
@@ -9,16 +9,22 @@ Header.propTypes = {
 
 function Header(props) {
     return (
-        <div className='Header'>
-            <div className="logo">
+        <div className='Header row'>
+            <div className="logo col-4">
                 <img src={'./images/ShopLogo/ShopLogo.png'} alt="Logo" />
                 <span>JAGERTHEJAGER SHOP</span>
             </div>
-            <div className="menus">
-                <button><Link to="/">About</Link></button>
+            <div className="menus col-8">
+                <ol>
+                    <li><Link to="/">About</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li><Link to="/">Shopping</Link></li>
+                    <li><Link to="/">Recipes</Link></li>
+                </ol>
+                {/* <button><Link to="/">About</Link></button>
                 <button><Link to="/products">Products</Link></button>
                 <button><Link to="/">Shopping</Link></button>
-                <button><Link to="/">Recipes</Link></button>
+                <button><Link to="/">Recipes</Link></button> */}
             </div>
         </div>
     );
