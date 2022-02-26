183a6bdfa08021754c075f68f7f5cd3b4b0fe904
TuanAn <nptatdt@gmail.com>
2022-02-27 03:16:12 +0700

    LandingPage content responsive

--- src/components/CommonComponents/Content/Content.jsx
+++ src/components/CommonComponents/Content/Content.jsx
@@ -8,8 +8,8 @@ Content.propTypes = {
 
 function Content(props) {
     return (
-        <div className='Content'>
-            <div className="content-left">
+        <div className='Content row'>
+            <div className="content-left col-6">
                 <h1>JagermeisterJagermeisterJagermeisterJagermeisterJagermeister JagermeisterJagermeister JagermeisterJagermeisterJagermeisterJagermeister </h1>
                 <p>JagermeisterJagermeisterJagermeisterJagermeisterJagermeisterJagermeisterJagermeisterJagermeisterJagermeister Jagermeister Jagermeister Jagermeister Jagermeister Jagermeister Jagermeister</p>
                 <div className="product-navigator">
@@ -17,7 +17,8 @@ function Content(props) {
                     <span>Let's get started ➔</span>
                 </div>
             </div>
-            <div className="content-right">
+            <div className="content-right col-6">
+                <img src={'./images/ShopLogo/ShopLogo.png'} alt="" />
             </div>
         </div>
     );
