59260184fa14320b1e47fb830dd30ac88485dcf3
TuanAn <nptatdt@gmail.com>
2022-02-27 03:01:28 +0700

    Footer responsive

--- src/components/CommonComponents/Footer/Footer.jsx
+++ src/components/CommonComponents/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
@@ -11,46 +10,54 @@ Footer.propTypes = {
 function Footer(props) {
     return (
         <div className='Footer'>
-            <div className="footer-title">
+            <div className="footer-title row">
                 <h1>Personal Information</h1>
             </div>
 
-            <div className="information">
+            <div className="information row">
                 <div className='footer-item'>
-                    <div className='outer-icon'>
+                    <div className='outer-icon col-3'>
                         <FontAwesomeIcon id='location' icon={solid('location-dot')} />
                     </div>
-                    <span>304 Nguyen Thuong Hien street, Ward 5, Phu nhuan District</span>
+                    <span className='col-9'>304 Nguyen Thuong Hien street, Ward 5, Phu nhuan District</span>
                 </div>
+
                 <div className='footer-item'>
-                    <div className='outer-icon'>
+                    <div className='outer-icon col-3'>
                         <FontAwesomeIcon icon={solid('phone')} />
                     </div>
-                    <span>0927183879 - 0818191099</span>
+                    <span className='col-9'>0927183879 - 0818191099</span>
                 </div>
-                <div className='footer-item'>
-                    <a href="https://www.facebook.com/jagerthejager" target="_blank">
-                        <div className='outer-icon'>
+
+                <a href="https://www.facebook.com/jagerthejager" target="_blank">
+                    <div className='footer-item'>
+                        <div className='outer-icon col-3'>
                             <FontAwesomeIcon icon={brands('facebook-f')} />
                         </div>
-                        <span>Fanpage (Click me)</span>
-                    </a>
-                    <a href="https://www.facebook.com/19.anim" target="_blank">
-                        <div className='outer-icon'>
+                        <span className='col-9'>Fanpage (Click me)</span>
+                    </div>
+                </a>
+
+                <a href="https://www.facebook.com/19.anim" target="_blank">
+                    <div className='footer-item'>
+                        <div className='outer-icon col-3'>
                             <FontAwesomeIcon icon={brands('facebook-f')} />
                         </div>
-                        <span>Personal Facebook (Click me)</span>
-                    </a>
-                </div>
-                <div className='footer-item'>
-                    <a href="https://www.tiktok.com/@jagerthejager" target="_blank">
-                        <div className='outer-icon'>
+                        <span className='col-9'>Personal Facebook (Click me)</span>
+                    </div>
+                </a>
+
+
+                <a href="https://www.tiktok.com/@jagerthejager" target="_blank">
+                    <div className='footer-item'>
+                        <div className='outer-icon col-3'>
                             <FontAwesomeIcon icon={brands('tiktok')} />
                         </div>
-                        <span>Tiktok for recipes and more (Click me)</span>
-                    </a>
-                </div>
+                        <span className='col-9'>Tiktok for recipes and more (Click me)</span>
+                    </div>
+                </a>
             </div>
+
         </div >
 
     );
